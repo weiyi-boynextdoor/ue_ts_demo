14b73a5d50e2966ad036fe0ca3c103725f85ae05
weiyi <weiyiweishao@163.com>
2025-07-06 11:01:03 +0800

    remove unused js

--- Content/JavaScript/InitOld.js
+++
@@ -1,6 +0,0 @@
-console.log("init from ts begin");
-require('./BP_ActorMixin');
-require('./BP_ThirdPersonCharacterMixin');
-require('./WBP_MainUIMixin');
-console.log("init from ts end");
-//# sourceMappingURL=InitOld.js.map
